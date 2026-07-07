6e1a5942636e950f81a6c83cf7065b182d5dcf59
Leticia Souza <leticiasouza.baptista05@gmail.com>
2026-07-07 17:10:01 -0300

    terminada a logica da atividade de alimentar

--- pets-digitais/src/main/java/br/trabalho/service/CriaturaService.java
+++ pets-digitais/src/main/java/br/trabalho/service/CriaturaService.java
@@ -6,23 +6,27 @@ import br.trabalho.model.Aquari;
 import br.trabalho.model.Criatura;
 import br.trabalho.model.Draconis;
 import br.trabalho.model.DraconisCelestial;
+import br.trabalho.model.Estoque;
 import br.trabalho.model.Fungari;
 import br.trabalho.model.Lumini;
 import br.trabalho.model.Mecanis;
+import br.trabalho.model.TipoAlimento;
 import br.trabalho.util.Leituras;
 import br.trabalho.repository.CriaturaRepository;
 
 public class CriaturaService {
 
     private CriaturaRepository repository;
+    private Estoque estoque;
 
     public CriaturaService(){
         repository = new CriaturaRepository();
+        estoque = new Estoque();
     }
 
     public void criaCriatura(){
 
-        System.out.print("Nome: ");
+        System.out.print("\nNome: ");
         String nome = Leituras.leFrase();
 
         System.out.print("╔════════════════════════╗\n");
@@ -44,36 +48,42 @@ public class CriaturaService {
             switch (opcao) {
             case 1:{
                 repository.salvaCriatura(new Draconis(nome));
+                System.out.println("\nCriatura criada com sucesso!");
                 repository.getCriatura(nome).exibeInformacoes();
                 opcaoValida = true;
                 break;
                 }
             case 2:{
                 repository.salvaCriatura(new Lumini(nome));
+                System.out.println("\nCriatura criada com sucesso!");
                 repository.getCriatura(nome).exibeInformacoes();
                 opcaoValida = true;
                 break;
                 }
             case 3:{
                 repository.salvaCriatura(new Fungari(nome));
+                System.out.println("\nCriatura criada com sucesso!");
                 repository.getCriatura(nome).exibeInformacoes();
                 opcaoValida = true;
                 break;
                 }
             case 4:{
                 repository.salvaCriatura(new Aquari(nome));
+                System.out.println("\nCriatura criada com sucesso!");
                 repository.getCriatura(nome).exibeInformacoes();
                 opcaoValida = true;
                 break;
                 }
             case 5:{
                 repository.salvaCriatura(new Mecanis(nome));
+                System.out.println("\nCriatura criada com sucesso!");
                 repository.getCriatura(nome).exibeInformacoes();
                 opcaoValida = true;
                 break;
                 }
             case 6:{
                 repository.salvaCriatura(new DraconisCelestial(nome));
+                System.out.println("\nCriatura criada com sucesso!");
                 repository.getCriatura(nome).exibeInformacoes();
                 opcaoValida = true;
                 break;
@@ -83,8 +93,92 @@ public class CriaturaService {
                 }
             }
         }
-
-        
     }
 
+    public void criaturasCadastradas(){
+
+        if(repository.getCriaturas().isEmpty())
+            System.out.println("O reino esá sem Pets");//melhorar essa frase
+        else{
+            for(Criatura criatura: repository.getCriaturas()){
+            System.out.println("------------------------");
+            System.out.println(criatura);
+            System.out.println("------------------------");
+            }
+        }        
+    }
+
+    public void removeCriatura(Criatura criatura){
+        repository.removeCriatura(criatura);
+    }
+
+    public void exibeInformacoes(Criatura criatura){
+        if(repository.criaturaExiste(criatura))
+            criatura.exibeInformacoes();
+        else
+            System.out.println("Criatura não encontrada no reino.");
+    }
+
+    public void exibeSatus(Criatura criatura){
+        if(repository.criaturaExiste(criatura))
+            criatura.exibeEstadoAtual();
+        else
+            System.out.println("Criatura não encontrada no reino.");
+    }
+
+    public void brincar(Criatura criatura){
+        if(repository.criaturaExiste(criatura)){
+            if(!criatura.brincar())
+                System.out.print("A criatura não está disposta pra brincar.");
+        }
+        else
+            System.out.println("Criatura não encontrada no reino.");
+    }
+
+    public void treinar(Criatura criatura){
+        if(repository.criaturaExiste(criatura)){
+            if(!criatura.treinar())
+                System.out.print("A criatura não está disposta pra treinar.");
+        }
+        else
+            System.out.println("Criatura não encontrada no reino.");
+    }
+
+    public void explorar(Criatura criatura){
+        if(repository.criaturaExiste(criatura)){
+            if(!criatura.explorar())
+                System.out.print("A criatura não está disposta pra explorar.");
+        }
+        else
+            System.out.println("Criatura não encontrada no reino.");
+    }
+
+    public void alimentar(Criatura criatura, TipoAlimento alimento){
+        if(repository.criaturaExiste(criatura)){
+            if(criatura.getTipoCriatura().podeComer(alimento)){
+                if(estoque.existeNoEstoque(alimento)){
+                    criatura.alimentar(alimento);
+                    estoque.reabastece();
+                }
+                else
+                    System.out.println("Alimento não esta no estoque.");
+            }
+            else
+                System.out.println("\nCriatura não pode comer este alimento.");
+        }
+        else
+            System.out.println("Criatura não encontrada no reino.");    
+    }
+
+    public void dormir(Criatura criatura){
+        repository.getCriatura(criatura.getNome()).brincar();
+    }
+
+    public void desafio(Criatura criatura){
+        repository.getCriatura(criatura.getNome()).brincar();
+    }
+
+    public void habilidadeEspecial(Criatura criatura){
+        repository.getCriatura(criatura.getNome()).brincar();
+    }
 }
