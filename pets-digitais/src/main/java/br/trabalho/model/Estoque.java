53fba75ee01779857dfc7d3c1529679ba7b65f52
Leticia Souza <leticiasouza.baptista05@gmail.com>
2026-07-07 16:54:47 -0300

    logica do estoque atualizado

--- pets-digitais/src/main/java/br/trabalho/model/Estoque.java
+++ pets-digitais/src/main/java/br/trabalho/model/Estoque.java
@@ -1,170 +1,87 @@
 package br.trabalho.model;
 
 import java.util.List;
+import java.util.Map;
+
+import br.trabalho.repository.CriaturaRepository;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Estoque {
 
-    private List<TipoAlimento> estoqueAquari;
-    private List<TipoAlimento> estoqueDraconis;
-    private List<TipoAlimento> estoqueDraconisCelestial;
-    private List<TipoAlimento> estoqueFungari;
-    private List<TipoAlimento> estoqueLumini;
-    private List<TipoAlimento> estoqueMecanis;
-    private int aquariEstoqueMinimo;
-    private int draconisEstoqueMinimo;
-    private int draconisCelestialEstoqueMinimo;
-    private int fungariEstoqueMinimo;
-    private int luminiEstoqueMinimo;
-    private int mecanisEstoqueMinimo;
+    private Map<TipoAlimento, Integer> estoque;
+    private CriaturaRepository repository;
 
     public Estoque(){
-        estoqueAquari = new ArrayList<>();
-        estoqueDraconis = new ArrayList<>();
-        estoqueDraconisCelestial = new ArrayList<>();
-        estoqueFungari = new ArrayList<>();
-        estoqueLumini = new ArrayList<>();
-        estoqueMecanis = new ArrayList<>();
+        estoque = new HashMap();
 
-        //adicionar alguns alimentos em cada lista pra iniciar
+        estoque.put(TipoAlimento.FRUTAS, 0);
+        estoque.put(TipoAlimento.CARNE, 0);
+        estoque.put(TipoAlimento.NECTARLUMINOSO, 0);
+        estoque.put(TipoAlimento.FOTONS, 0);
+        estoque.put(TipoAlimento.COGUMELOS, 0);
+        estoque.put(TipoAlimento.CRISTAISENERGETICOS, 0);
+        estoque.put(TipoAlimento.BANQUETEREAL, 0);
     }
 
-    public void aquariEstoqueMinimo(){
-        aquariEstoqueMinimo = Aquari.getTotalAquari() * 3;
+    public int getQuantidadeEstoque(){
+        return estoque.size();
+        //se tiver vazio
     }
 
-    public void draconisEstoqueMinimo(){
-        draconisEstoqueMinimo = Draconis.getTotalDraconis() * 3;
+    public void imprimeEstoque(){
+        System.out.println("\nFrutas: " + estoque.get(TipoAlimento.FRUTAS) + 
+        "\nCarne " + estoque.get(TipoAlimento.CARNE) + 
+        "\nFotons: " + estoque.get(TipoAlimento.FOTONS) + 
+        "\nNectar Luminoso: " + estoque.get(TipoAlimento.NECTARLUMINOSO) + 
+        "\nCogumelos: " + estoque.get(TipoAlimento.COGUMELOS) +
+        "\nCristais Energéticos: " + estoque.get(TipoAlimento.CRISTAISENERGETICOS) + 
+        "\nBanquete Real: " + estoque.get(TipoAlimento.BANQUETEREAL));
     }
 
-    public void draconisCelestialEstoqueMinimo(){
-        draconisCelestialEstoqueMinimo = DraconisCelestial.getTotalDraconisCelestial() * 3;
+    public boolean existeNoEstoque(TipoAlimento alimento){
+        if(estoque.get(alimento) > 0)
+            return true;
+        return false;
     }
 
-    public void FungariEstoqueMinimo(){
-        fungariEstoqueMinimo = Fungari.getTotalFungari() * 3;
-    }
+    public void reabastece() {
 
-    public void LuminiEstoqueMinimo(){
-        luminiEstoqueMinimo = Lumini.getTotalLumini() * 3;
-    }
+        for (TipoCriatura criatura: TipoCriatura.values()) {
 
-    public void MecanisEstoqueMinimo(){
-        mecanisEstoqueMinimo = Mecanis.getTotalMecanis() * 3;
-    }
+            int minimo = repository.quantidadeCriaturas(criatura) * 3;
 
-    public int quantidadeDisponivel(TipoCriatura criatura){
+            int disponivel = calculaEstoqueDisponivel(criatura);
 
-        switch (criatura) {
-            case AQUARI: 
-                return estoqueAquari.size();
-            case DRACONIS: 
-                return estoqueDraconis.size();
-            case DRACONISCELESTIAL: 
-                return estoqueDraconisCelestial.size();
-            case FUNGARI: 
-                return estoqueFungari.size();
-            case LUMINI: 
-                return estoqueLumini.size();
-            case MECANIS: 
-                return estoqueMecanis.size();
-        
-            default:
-                return 0;
+            if (disponivel < minimo) {
+                adicionaAlimentos(criatura);
+            }
         }
     }
 
-    public void reabasteceAquari(){
+    private int calculaEstoqueDisponivel(TipoCriatura criatura){
 
-        for(int i = 0; i<4; i++){
-            estoqueAquari.add(TipoAlimento.NECTARLUMINOSO);
-        }
+        int total = 0;
 
-        for(int i = 0; i<4; i++){
-            estoqueAquari.add(TipoAlimento.CRISTAISENERGETICOS);
-        }
+        for(TipoAlimento alimento : criatura.getAlimentosPermitidos())
+            total += estoque.get(alimento);
 
-        for(int i = 0; i<2; i++){
-            estoqueAquari.add(TipoAlimento.BANQUETEREAL);
-        }
+        return total;
     }
 
-    public void reabasteceDraconis(){
+    private void adicionaAlimentos(TipoCriatura especie){
 
-        
-        for(int i = 0; i<4; i++){
-            estoqueDraconis.add(TipoAlimento.FRUTAS);
-        }
+        List<TipoAlimento> alimentos = especie.getAlimentosPermitidos();
 
-        
-        for(int i = 0; i<4; i++){
-            estoqueDraconis.add(TipoAlimento.CARNE);
-        }
+        int quantidadePorAlimento = 10 / alimentos.size();
 
-        
-        for(int i = 0; i<2; i++){
-            estoqueDraconis.add(TipoAlimento.BANQUETEREAL);
+        for(TipoAlimento alimento : alimentos){
+
+            estoque.put(
+                alimento,
+                estoque.get(alimento) + quantidadePorAlimento
+            );
         }
     }
-
-    public void reabasteceDraconisCelestial(){
-
-        for(int i = 0; i<4; i++){
-            estoqueDraconisCelestial.add(TipoAlimento.CARNE);
-        }
-
-        for(int i = 0; i<4; i++){
-            estoqueDraconisCelestial.add(TipoAlimento.FOTONS);
-        }
-        
-        for(int i = 0; i<2; i++){
-            estoqueDraconisCelestial.add(TipoAlimento.BANQUETEREAL);
-        }
-    }
-
-    public void reabasteceFungari(){
-
-        for(int i = 0; i<4; i++){
-            estoqueFungari.add(TipoAlimento.FRUTAS);
-        }
-
-        for(int i = 0; i<4; i++){
-            estoqueFungari.add(TipoAlimento.COGUMELOS);
-        }
-
-        for(int i = 0; i<2; i++){
-            estoqueFungari.add(TipoAlimento.BANQUETEREAL);
-        }
-    }
-
-    public void reabasteceLumini(){
-
-        for(int i = 0; i<4; i++){
-            estoqueLumini.add(TipoAlimento.FOTONS);
-        }
-
-        for(int i = 0; i<4; i++){
-            estoqueLumini.add(TipoAlimento.NECTARLUMINOSO);
-        }
-
-        for(int i = 0; i<2; i++){
-            estoqueLumini.add(TipoAlimento.BANQUETEREAL);
-        }
-    }
-
-    public void reabasteceMecanis(){
-
-        for(int i = 0; i<4; i++){
-            estoqueMecanis.add(TipoAlimento.COGUMELOS);
-        }
-
-        for(int i = 0; i<4; i++){
-            estoqueMecanis.add(TipoAlimento.CRISTAISENERGETICOS);
-        }
-        
-        for(int i = 0; i<2; i++){
-            estoqueMecanis.add(TipoAlimento.BANQUETEREAL);
-        }
-    }
-    
 }
